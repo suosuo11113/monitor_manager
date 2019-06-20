32ebdc47fb9687b07259b6ee5bea4a5ad0f8d6d5
suosuo11113 <yifengdanqing@126.com>
2019-06-20 11:04:19 +0800

    readme: add dingding 接口说明 www.views： 修改编码格式 unicode-escape 改为 GBK

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # monitor_manager
+功能1：使用钉钉通知接口，主要分为3个等级
+1. info : curl -X POST -s http://127.0.0.1:8000/dingding/info -d "test info"
+2. warn : curl -X POST -s http://127.0.0.1:8000/dingding/warn -d "test warn"
+3. error: curl -X POST -s http://127.0.0.1:8000/dingding/error -d "test error"
+
+
--- www/www/views.py
+++ www/www/views.py
@@ -6,7 +6,6 @@ from django.http import HttpResponse
 os.chdir(os.path.split(os.path.realpath(sys.argv[0]))[0])
 sys.path.append("../")
 from  dingding import Dingding
-import chardet
 
 
 
@@ -23,7 +22,7 @@ def dingding_info(req):
 def dingding_warn(req):
     xiaoding = Dingding.Drobot("warn")
     if req.method == "POST":
-        msg = req.body.decode('unicode-escape')
+        msg = req.body.decode('GBK')
         xiaoding.send_text(msg)
     return HttpResponse()
 
@@ -31,7 +30,7 @@ def dingding_warn(req):
 def dingding_error(req):
     xiaoding = Dingding.Drobot("error")
     if req.method == "POST":
-        msg = req.body.decode('unicode-escape')
+        msg = req.body.decode('GBK')
         xiaoding.send_text(msg)
     return HttpResponse()
 
